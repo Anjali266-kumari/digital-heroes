7cd27d0a53852e2047d2792539c7b2821e71245d
Anjali266-kumari <anjalikumari25012@gmail.com>
2026-09-22 20:49:01 +0530

    Fix User model import in charity route

--- backend/routes/Charity.js
+++ backend/routes/Charity.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Charity from "../models/Charity.js";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
